2a857ec9e5b9f4b50facd0bb208bc12712591abe
alexkemboi4127 <alexkemboi97@yahoo.com>
2023-01-26 23:18:37 +0300

    node.js app to make an stk push

---
+++ index.js
@@ -0,0 +1,62 @@
+class Stanbic {
+  constructor() {
+    this.consumerKey = "a01479ca2e0bdee0510f29988e8e1130";
+    this.consumerSecret = "6fa98994067428c7047f5519ad3e2b46";
+    this.accessToken = "";
+  }
+
+  async getToken() {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "client_id=" +
+        this.consumerKey +
+        "&client_secret=" +
+        this.consumerSecret +
+        "&scope=payments&grant_type=client_credentials",
+    };
+
+    const response = await fetch(
+      "https://api.connect.stanbicbank.co.ke/api/sandbox/auth/oauth2/token",
+      options
+    );
+    const json = await response.json();
+    this.accessToken = json.access_token;
+    return this.accessToken;
+  }
+
+  async makePayment(token, phone, amount) {
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        dbsReferenceId: "REW21331DR5F1",
+        billAccountRef: "3333562174",
+        amount: amount,
+        mobileNumber: phone,
+        corporateNumber: "740757",
+        bankReferenceId: "0100010969764",
+        txnNarrative: "ttsteeeee",
+        callBackUrl: "",
+      }),
+    };
+
+    const response = await fetch(
+      "https://api.connect.stanbicbank.co.ke/api/sandbox/mpesa-checkout",
+      options
+    );
+    const json = await response.json();
+    console.log(json);
+  }
+}
+
+const stanbic = new Stanbic();
+stanbic
+  .getToken()
+  .then((token) => stanbic.makePayment(token, "254719835499", "100"));
